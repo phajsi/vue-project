57af39e1190bdb83a7a23fec63f0792019af86e1
phajsi <phajsi.choque.halvorsen@nrk.no>
2024-04-19 13:32:56 +0200

    Staring Vue project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="This is a site created with Vue 3" />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
